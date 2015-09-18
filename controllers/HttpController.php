1f7311bc1ad497d2f69af052af173bd2b162e021
hucongyang <731084626@email.com>
2015-09-18 14:06:54 +0800

    增加大汉三通发送短信验证码接口

--- controllers/HttpController.php
+++ controllers/HttpController.php
@@ -6,8 +6,8 @@ class HttpController extends ApiPublicController
     {
         $account = "dh21317";       // 账号
         $password = "64524df26b97a865accddfa4e8b3b684";         // 密码
-        $phone = "13918231466";     // 号码
-        $content = "短信测试正在进行，打扰你开会，抱歉";           // 内容
+        $phone = "15201920325";     // 号码
+        $content = "您好，您的验证码为1111";           // 内容
         $sign = "";                     // 短信签名
         $sendTime = "";                 // 发送时间
 
@@ -44,5 +44,53 @@ class HttpController extends ApiPublicController
 //        echo $contents;
 
         $this->_return("MSG_SUCCESS", $contents);
+    }
+
+    public function actionBaiduXunke()
+    {
+        $nowTime = date("Y-m-d H:i:s");
+//        $fileContent = file_get_contents("php://input");
+        $xml = simplexml_load_string("<?xml version='1.0' encoding='UTF-8'?><TXTbaidu><TXTbaiduRequest><userId>cnhutong</userId><password>111111</password></TXTbaiduRequest><body><tempBase><Mediasource>baidu-xunke</Mediasource><Device>Wise</Device><Query>英语培训哪里好</Query><cityName>上海</cityName><cityCode>300310</cityCode><teleNum> 13500998811</teleNum><Name>XXX先生</Name><Class>英语冲刺班</Class></tempBase></body></TXTbaidu>");
+//        var_dump($fileContent);
+//        var_dump($xml);
+        $userId = (string)$xml->TXTbaiduRequest->userId;
+        $password = (string)$xml->TXTbaiduRequest->password;
+        $mediaSource = (string)$xml->body->tempBase->Mediasource;
+//        var_dump($mediaSource);exit;
+        if($userId == 'cnhutong' && $password == '111111' && $mediaSource == 'baidu-xunke')
+        {
+            $device = (string)$xml->body->tempBase->Device;
+            $Query = (string)$xml->body->tempBase->Query;
+            $cityName = (string)$xml->body->tempBase->cityName;
+            $cityCode = (string)$xml->body->tempBase->cityCode;
+            $teleNum = (string)$xml->body->tempBase->teleNum;
+            $name = (string)$xml->body->tempBase->Name;
+            $class = (string)$xml->body->tempBase->Class;
+
+            try {
+                $result = Yii::app()->cnhutong_user->createCommand()
+                    ->insert('baidu_xunke',
+                        array(
+                            'device' => $device,
+                            'query' => $Query,
+                            'cityName' => $cityName,
+                            'cityCode' => $cityCode,
+                            'teleNum' => $teleNum,
+                            'name' => $name,
+                            'class' => $class,
+                            'create_ts' => $nowTime
+                        )
+                    );
+
+                if(!$result) {
+                    $this->_return("MSG_SUCCESS", '数据库插入数据错误');
+                }
+                $this->_return("MSG_SUCCESS", $result);
+            } catch (Exception $e) {
+                error_log($e);
+            }
+        } else {
+            $this->_return("MSG_SUCCESS", '用户名密码错误');
+        }
     }
 }
--- models/LogMobileCheckcode.php
+++ models/LogMobileCheckcode.php
@@ -131,7 +131,151 @@ class LogMobileCheckcode extends CActiveRecord
             return 10002;       //MSG_ERR_FAIL_PARAM
         }
 
+        // 大汉三通发送验证码短信接口实现
+        $account = "dh21317";       // 账号
+        $password = "64524df26b97a865accddfa4e8b3b684";         // 密码
+        $phone = "15201920323";     // 号码
+        $content = "您好，您的验证码为1111";           // 内容
+        $sign = "";                     // 短信签名
+        $sendTime = "";                 // 发送时间
+        $contentXml = self::postCheckNum($account, $password, $phone, $content, $sign, $sendTime);
+        // 获取发送短信响应状态码
+        $status = self::getStatusByXml($contentXml);
+//        $status = explode("</result>", explode("<result>", $contentXml)[1])[0];
+//        var_dump($status);
+//        var_dump($contentXml);exit;
+        // 记录发送短信日志
+        self::insertPostCheckNum($mobile, $nowTime, $status, $type);
+
         return $checkNum;       // 返回的验证码目前测试用，正式上线去除
+    }
+
+    /**
+     * 记录发送短信日志
+     * @param $mobile
+     * @param $send_ts
+     * @param $status
+     * @param $send_type
+     * @return int
+     */
+    public function insertPostCheckNum($mobile, $send_ts, $status, $send_type)
+    {
+        $result = 0;
+        try {
+            $result = Yii::app()->cnhutong_user->createCommand()
+                ->insert('log_sms_send_history',
+                    array(
+                        'mobile' => $mobile,
+                        'send_ts' => $send_ts,
+                        'status' => $status,
+                        'send_type' => $send_type
+                    )
+                );
+        } catch (Exception $e) {
+            error_log($e);
+        }
+        return $result;
+    }
+
+    /**
+     * 大汉三通短信云接口手册V1.3.4
+     * 4.2.1 短信下行
+     * 访问地址：http://http://3tong.net/http/sms/Submit
+     * 提交方式：POST
+     * @param $account                      -- 用户帐号
+     * @param $password                     -- 账号密码
+     * @param $phone                        -- 接手手机号码，多个手机号码用英文逗号分隔，最多500个
+     * @param $content                      -- 短信内容，最多350个汉字（包含签名长度）
+     * @param string $sign                  -- 短信签名，默认为 【秦汉胡同】
+     * @param string $sendTime              -- 定时发送时间 格式yyyyMMddHHmm 为空或早于当前时间则立即发送
+     * @return string                       -- xml
+     */
+    public function postCheckNum($account, $password, $phone, $content, $sign = "", $sendTime = "")
+    {
+        $sendSmsAddress = "http://3tong.net/http/sms/Submit";
+        $message ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            ."<message>"
+            . "<account>"
+            . $account
+            . "</account><password>"
+            . $password
+            . "</password>"
+            . "<msgid></msgid><phones>"
+            . $phone
+            . "</phones><content>"
+            . $content
+            . "</content><subcode>"
+            ."</subcode><sign>"
+            . $sign
+            . "</sign><sendtime>"
+            . $sendTime
+            . "</sendtime>"
+            ."</message>";
+
+        $params = array(
+            'message' => $message);
+
+        $data = http_build_query($params);
+        $context = array('http' => array(
+            'method' => 'POST',
+            'header'  => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => $data,
+        ));
+        $contents = file_get_contents($sendSmsAddress, false, stream_context_create($context));
+        return $contents;
+    }
+
+    /**
+     * 解析短信响应状态码
+     * @param $stringXml
+     * @return string
+     */
+    public function getStatusByXml($stringXml)
+    {
+        $xml = simplexml_load_string($stringXml);
+        $status = (string)$xml->result;
+        return $status;
+    }
+
+    /**
+     * 短信提交响应错误码
+     * @param $status
+     * @return string
+     */
+    public function statusDesc($status)
+    {
+        switch ($status) {
+            case 0:
+                return "提交成功";
+            case 1:
+                return "账号无效";
+            case 2:
+                return "密码错误";
+            case 3:
+                return "msgid太长，不得超过32位";
+            case 5:
+                return "手机号码个数超过最大限制（500个）";
+            case 6:
+                return "短信内容超过最大限制（350字）";
+            case 7:
+                return "扩展子号码无效";
+            case 8:
+                return "定时时间格式错误";
+            case 14:
+                return "手机号码为空";
+            case 19:
+                return "用户被禁发或禁用";
+            case 20:
+                return "ip鉴权失败";
+            case 21:
+                return "短信内容为空";
+            case 22:
+                return "数据包大小不匹配";
+            case 98:
+                return "系统正忙";
+            case 99:
+                return "消息格式错误";
+        }
     }
 
     /**
